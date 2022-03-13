5f370d472587d7d4fe1cbb5d9cfe75a2b443c7ee
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-13 14:46:51 +0300

    bug:remove toggle effect bug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //
 $(document).ready(function(){
     var services=[{img:"design_image",details:"design_details"},
-                  {img:"development_image",details:"design_details"},
+                  {img:"development_image",details:"development_details"},
                   {img:"product_image",details:"product_details"}]
 
    services.forEach(function(service){
