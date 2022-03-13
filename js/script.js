30eccbe7bc91bd1c41a04f03e62ef0559cc4649b
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-13 14:42:08 +0300

    ft:add toggle effect on what_we_do section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//
+$(document).ready(function(){
+    var services=[{img:"design_image",details:"design_details"},
+                  {img:"development_image",details:"design_details"},
+                  {img:"product_image",details:"product_details"}]
+
+   services.forEach(function(service){
+    $("." +service.img).click(function(){
+        $("."+service.details).show();
+        $("."+service.img).hide();
+    });
+   $("."+service.details).click(function(){
+        $("."+service.img).show();
+        $("."+service.details).hide();
+    });
+   })
+}); 
